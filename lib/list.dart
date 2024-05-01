50486115f6ec8f224c624b8ede3f711ef65b29f2
jinhyeon-dev <kjh101807@gmail.com>
2024-05-01 12:13:01 +0900

    commit

--- lib/list.dart
+++ lib/list.dart
@@ -1,112 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:mysql_client/mysql_client.dart';
-//
-// class MemberListScreen extends StatefulWidget {
-//   const MemberListScreen({Key? key}) : super(key: key);
-//
-//   @override
-//   _MemberListScreenState createState() => _MemberListScreenState();
-// }
-//
-// class _MemberListScreenState extends State<MemberListScreen> {
-//   List<Map<String, dynamic>> _members = [];
-//   bool _isLoading = true;
-//
-//   @override
-//   void initState() {
-//     super.initState();
-//     _loadMembers();
-//   }
-//
-//   Future<void> _loadMembers() async {
-//     print("Connecting to mysql server...");
-//
-//     // MySQL 접속 설정
-//     final conn = await MySQLConnection.createConnection(
-//       host: 'demo.cnyscyowakl3.ap-northeast-2.rds.amazonaws.com',
-//       port: 3306,
-//       userName: 'admin',
-//       password: 'aws-rds-jinhyeon',
-//       databaseName: 'demo', // optional
-//     );
-//
-//     await conn.connect();
-//
-//     print("Connected");
-//
-//     final result = await conn.execute("SELECT * FROM memberTable");
-//
-//     if (result != null && result.isNotEmpty) {
-//       setState(() {
-//         _members = result.rows.map((row) => row.assoc()).toList();
-//         _isLoading = false; // 데이터 로딩 완료
-//       });
-//     }
-//
-//     await conn.close();
-//   }
-//
-//   Future<void> _deleteMember(String memberId) async {
-//     // memberId를 int로 파싱
-//     final int parsedMemberId = int.tryParse(memberId) ?? 0;
-//
-//     // MySQL 접속 설정
-//     final conn = await MySQLConnection.createConnection(
-//       host: 'demo.cnyscyowakl3.ap-northeast-2.rds.amazonaws.com',
-//       port: 3306,
-//       userName: 'admin',
-//       password: 'aws-rds-jinhyeon',
-//       databaseName: 'demo', // optional
-//     );
-//
-//     await conn.connect();
-//
-//     print("Connected");
-//
-//     // 회원 삭제 쿼리 실행
-//     await conn.execute("DELETE FROM memberTable WHERE id = :id", {"id": parsedMemberId});
-//
-//     print("Member deleted successfully");
-//
-//     await conn.close();
-//
-//     // 삭제 후 화면 갱신
-//     setState(() {
-//       _members.removeWhere((member) => member['id'] == parsedMemberId);
-//     });
-//   }
-//
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Member List'),
-//       ),
-//       body: _isLoading
-//           ? Center(
-//         child: CircularProgressIndicator(), // 로딩 표시
-//       )
-//           : ListView.builder(
-//         itemCount: _members.length,
-//         itemBuilder: (context, index) {
-//           final member = _members[index];
-//           return ListTile(
-//             title: Text(member['name']),
-//             subtitle: Text(member['id'].toString()),
-//             trailing: IconButton(
-//               icon: Icon(Icons.delete),
-//               onPressed: () {
-//                 _deleteMember(member['id']);
-//               },
-//             ),
-//           );
-//         },
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:mysql_client/mysql_client.dart';
 
@@ -153,6 +44,7 @@ class _MemberListScreenState extends State<MemberListScreen> {
   }
 
   Future<void> _deleteMember(String memberId) async {
+    _refreshData();
     final int parsedMemberId = int.tryParse(memberId) ?? 0;
     final conn = await MySQLConnection.createConnection(
       host: 'demo.cnyscyowakl3.ap-northeast-2.rds.amazonaws.com',
@@ -163,11 +55,10 @@ class _MemberListScreenState extends State<MemberListScreen> {
     );
 
     await conn.connect();
-    await conn.execute("DELETE FROM memberTable WHERE id = :id", {"id": parsedMemberId});
+    await conn.execute(
+        "DELETE FROM memberTable WHERE id = :id", {"id": parsedMemberId});
     print("Member deleted successfully");
     await conn.close();
-
-    _refreshData();
   }
 
   Future<void> _refreshData() async {
@@ -190,24 +81,24 @@ class _MemberListScreenState extends State<MemberListScreen> {
       ),
       body: _isLoading
           ? Center(
-        child: CircularProgressIndicator(),
-      )
+              child: CircularProgressIndicator(),
+            )
           : ListView.builder(
-        itemCount: _members.length,
-        itemBuilder: (context, index) {
-          final member = _members[index];
-          return ListTile(
-            title: Text(member['name']),
-            subtitle: Text(member['id'].toString()),
-            trailing: IconButton(
-              icon: Icon(Icons.delete),
-              onPressed: () {
-                _deleteMember(member['id'].toString());
+              itemCount: _members.length,
+              itemBuilder: (context, index) {
+                final member = _members[index];
+                return ListTile(
+                  title: Text(member['name']),
+                  subtitle: Text(member['id'].toString()),
+                  trailing: IconButton(
+                    icon: Icon(Icons.delete),
+                    onPressed: () {
+                      _deleteMember(member['id'].toString());
+                    },
+                  ),
+                );
               },
             ),
-          );
-        },
-      ),
     );
   }
 }
